3f4a5347b2fcb141e727ff389b6d4e7aeb5478ee
sgoh <sgoh@nkia.co.kr>
2018-07-26 17:51:23 +0900

    issues #32 development

---
+++ src/components/icon/Icons.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Icons extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Icons;
