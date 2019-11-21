1cc795f307b7a9ffe37b21fa4a074f21fbcffc0e
salgum1114 <salgum1112@gmail.com>
2019-11-21 15:06:29 +0900

    [#131] updated docs

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.457674fdf1f481cd83d6719493854a66.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.c1f226901d70ea909dcb26db40e9c930.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.skipWaiting();
 workbox.clientsClaim();
