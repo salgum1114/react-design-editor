177f2d955d01048876477cf73f9e6c6aebbf5131
salgum1114 <salgum1112@gmail.com>
2019-12-08 15:29:44 +0900

    [#134] Adding chart option

---
+++ docs/precache-manifest.0705d5e4e671c43f9f90527bedd13623.js
@@ -0,0 +1,397 @@
+self.__precacheManifest = [
+  {
+    "url": "./fonts/a80a033f0cf0b8186287fa277c334efb.woff2"
+  },
+  {
+    "revision": "bf1b9ce6150e4e48ae4c",
+    "url": "./js/app.bf1b9ce6150e4e48.js"
+  },
+  {
+    "url": "./fonts/6537892df3329ee8024ec1bd09ca6122.woff"
+  },
+  {
+    "revision": "62a05f940c0dd3d1fec2",
+    "url": "./js/2.62a05f940c0dd3d1.js"
+  },
+  {
+    "revision": "4611f5a810ccc9d914aa",
+    "url": "./js/3.4611f5a810ccc9d9.js"
+  },
+  {
+    "url": "./fonts/47c9d192065844298d0c77e8d9b5728b.ttf"
+  },
+  {
+    "revision": "93068f826ac584e444b3a9af8a268238",
+    "url": "./index.html"
+  },
+  {
+    "revision": "52c8b678524e21208031",
+    "url": "./js/1.52c8b678524e2120.js"
+  },
+  {
+    "url": "./fonts/f1a4a058fbba1e35a406188ae7eddaf8.woff2"
+  },
+  {
+    "url": "./fonts/e6c93cb47e716b579264a5fdfbf7e84d.eot"
+  },
+  {
+    "url": "./fonts/f94f84eb867a3e3d65da0c5cad065437.woff"
+  },
+  {
+    "url": "./fonts/e92cc0fb9e1a7debc138224fd02a462a.woff2"
+  },
+  {
+    "url": "./fonts/f15aa285863274b4f6ed578caa76565e.woff2"
+  },
+  {
+    "url": "./fonts/f29d2b8559699b6beb5b29b25b8bc572.woff"
+  },
+  {
+    "url": "./fonts/f936cb550d4dcd769f75c453207ac5e6.woff2"
+  },
+  {
+    "url": "./fonts/ea363ed422723673917901680be9b37c.eot"
+  },
+  {
+    "url": "./fonts/e8019d507e8cb51d169ab4f94a0cda12.eot"
+  },
+  {
+    "url": "./fonts/f4b9b463353abe1b74ae6eb858e1a27c.svg"
+  },
+  {
+    "url": "./fonts/f08106ea0096fd585906ee8484b5469c.ttf"
+  },
+  {
+    "url": "./fonts/ee1e456d475733f7e8f45ae77eeed952.woff"
+  },
+  {
+    "url": "./fonts/f8c99dce09e7109c4f20a09c92da123e.woff2"
+  },
+  {
+    "url": "./fonts/f79929bb75161e13a5e457dfdd50c75b.svg"
+  },
+  {
+    "url": "./fonts/fdf44bc43e8fa2358bbb7d9165d78455.ttf"
+  },
+  {
+    "url": "./fonts/4286ac66d431783eb1c0e67e9cab5a71.svg"
+  },
+  {
+    "url": "./fonts/c2a4d575c73721ba88c000c74a7995b4.eot"
+  },
+  {
+    "url": "./fonts/c5ccf8984c0cb65692aed33f7af5945c.ttf"
+  },
+  {
+    "url": "./fonts/d75507313e9d71263646fc2ee4ae2af2.woff2"
+  },
+  {
+    "url": "./fonts/c8d109147435d76b46090824e1b5fced.ttf"
+  },
+  {
+    "url": "./fonts/c7792c7e9e55ca9ce80b34d181e16609.eot"
+  },
+  {
+    "url": "./fonts/ca70f49a133f08485bd05d5cb28ef8b7.woff"
+  },
+  {
+    "url": "./fonts/da408238128b876cbda6424391f1566f.woff"
+  },
+  {
+    "url": "./fonts/ddaaf4901aa3a2fa8805a235a7790f1f.svg"
+  },
+  {
+    "url": "./fonts/cffb686d7d2f4682df8342bd4d276e09.woff2"
+  },
+  {
+    "url": "./fonts/df2a321f01fad21758777e44fa839ccf.svg"
+  },
+  {
+    "url": "./fonts/dad90637f797356bbc70d2664832e0b6.woff"
+  },
+  {
+    "url": "./fonts/cdc8a9f118ddbf85ef5d8c06cfe042fc.ttf"
+  },
+  {
+    "url": "./fonts/cd17140fa66c29c783c63f9b0d956a82.svg"
+  },
+  {
+    "url": "./fonts/cf4574fbce791ed7debe1dee7f816283.svg"
+  },
+  {
+    "url": "./fonts/cdf5825996af9d722b115f7baf44ac61.ttf"
+  },
+  {
+    "url": "./fonts/e1ae1a0cb51d328d9b8e0426f48b9be9.ttf"
+  },
+  {
+    "url": "./fonts/da4b79be8c588f56351e4d6368fcdbe1.ttf"
+  },
+  {
+    "url": "./fonts/dd0cf7cdb6d5e97eb149f214b9cc2f2b.eot"
+  },
+  {
+    "url": "./fonts/e2bf8553e3677cca25294dbc749998a4.svg"
+  },
+  {
+    "url": "./fonts/d1ee74f2aeb877c324036f09349a2d67.woff2"
+  },
+  {
+    "url": "./fonts/d1093cd249095805312d099df6c7792e.svg"
+  },
+  {
+    "url": "./fonts/8d220c793e2612bd131ed8522c54669f.ttf"
+  },
+  {
+    "url": "./fonts/42c09d050c1e933066569515b037bf41.eot"
+  },
+  {
+    "url": "./fonts/7ddd3199cdace973375f71788d285acd.ttf"
+  },
+  {
+    "url": "./fonts/754c63cd1426bff4a89d9bacffc5b883.svg"
+  },
+  {
+    "url": "./fonts/87c3cd7999e540b8410825f34478d5a1.ttf"
+  },
+  {
+    "url": "./fonts/635ab018e0ab2b2467eb5731e615eabf.woff"
+  },
+  {
+    "url": "./fonts/76d8cbb0496cb184eff868152b67ad45.woff2"
+  },
+  {
+    "url": "./fonts/8d59c69546caed56e257ce10b9b22704.svg"
+  },
+  {
+    "url": "./fonts/9f732316066bd82f6d66c4f9de79ecfe.eot"
+  },
+  {
+    "url": "./fonts/bafb105baeb22d965c70fe52ba6b49d9.woff"
+  },
+  {
+    "url": "./fonts/7170668e2933c9f9e3ca64acd2c353a1.ttf"
+  },
+  {
+    "url": "./fonts/5d4aeb4e5f5ef754e307d7ffaef688bd.woff2"
+  },
+  {
+    "url": "./fonts/7678707a2677cab3caf55be262d473e1.woff2"
+  },
+  {
+    "url": "./fonts/9dce7f01715340861bdb57318e2f3fdc.eot"
+  },
+  {
+    "url": "./fonts/7a1be40ea00e8fa69b6eda262ee8a023.eot"
+  },
+  {
+    "url": "./fonts/6c423bb0f9e524c7f3ba9704aa23d6f7.woff"
+  },
+  {
+    "url": "./fonts/b37e5b6be439f6de427ebf2ed01b7724.woff2"
+  },
+  {
+    "url": "./fonts/8817cc38b4c986fcf46c6a5a420a8321.svg"
+  },
+  {
+    "url": "./fonts/5f5d6d4341032fa3e65f7080ab79fb88.ttf"
+  },
+  {
+    "url": "./fonts/8e89b7d5a1c0a024e94eaa64ba3092a8.eot"
+  },
+  {
+    "url": "./fonts/6d0694241dafa23fa07d7cfcf64f3dd7.eot"
+  },
+  {
+    "url": "./fonts/a33f5bf82e263ebbf148b6518563a8a6.woff"
+  },
+  {
+    "url": "./fonts/9d0333d253ab26a847750bea12ef067c.eot"
+  },
+  {
+    "url": "./fonts/6f861e953e80763462f1a27a08dd7c91.svg"
+  },
+  {
+    "url": "./fonts/76a43cc8ad247704f7c0110d7c24ae8e.woff2"
+  },
+  {
+    "url": "./fonts/62fb51e9e645f63599238881b9de15dd.woff"
+  },
+  {
+    "url": "./fonts/61f3dc557b73138d5721767af3a2386e.svg"
+  },
+  {
+    "url": "./fonts/c03588a7704f2804f6c9be291068916d.ttf"
+  },
+  {
+    "url": "./fonts/6a6d715087a68ac5ad790b4f7bbb1766.eot"
+  },
+  {
+    "url": "./fonts/ab0616e6d856817eff54eb8c5e1664e4.woff"
+  },
+  {
+    "url": "./fonts/944f2c386cf0e2d01d70031ccdfdcc5f.eot"
+  },
+  {
+    "url": "./fonts/b76febefc08cb94a5fd24fa1cecbd382.eot"
+  },
+  {
+    "url": "./fonts/501ce09c42716a2f6e1503a25eb174c9.woff2"
+  },
+  {
+    "url": "./fonts/ad3b964ac1560bb0046a929ba21b0b23.eot"
+  },
+  {
+    "url": "./fonts/68889c246da2739681c1065d15a1ab0b.eot"
+  },
+  {
+    "url": "./fonts/b75b4bfe0d58faeced5006c785eaae23.woff2"
+  },
+  {
+    "url": "./fonts/abc24b0f77dd16d0b4ea7cbe4a1082b0.woff2"
+  },
+  {
+    "url": "./fonts/4bdffaaa646258ee12f4303664e3c806.woff"
+  },
+  {
+    "url": "./fonts/4cd101b120e15f4d64f73a6bf87fbac7.ttf"
+  },
+  {
+    "url": "./fonts/a8507c4e8bf31f5835b717930acf6f6d.svg"
+  },
+  {
+    "url": "./fonts/bd67f25d9c25994ffde79d2a81b85a66.woff"
+  },
+  {
+    "url": "./fonts/693015cfe3fcf90e190a4062559e2c84.woff"
+  },
+  {
+    "url": "./fonts/4ffa4616720ccdaebdf6002f6839a326.ttf"
+  },
+  {
+    "url": "./fonts/8d9ab84bfe87a3f77112a6698cf639fb.woff2"
+  },
+  {
+    "url": "./fonts/9476342c377e7e14efa2f5a3a5599bd0.svg"
+  },
+  {
+    "url": "./fonts/bf2d0783515b7d75c35bde69e01b3135.woff"
+  },
+  {
+    "url": "./fonts/c045b73d86803686f4cd1cc3f9ceba59.ttf"
+  },
+  {
+    "url": "./fonts/60264058b23ab6faa42699a5e0035dd0.woff"
+  },
+  {
+    "url": "./fonts/52d369b0ef9f3a7b5d6a863d3597bcb1.eot"
+  },
+  {
+    "url": "./fonts/6814d0e8136d34e313623eb7129d538e.woff2"
+  },
+  {
+    "url": "./fonts/8b25b179c042d2e1f6bea86369062c4d.woff2"
+  },
+  {
+    "url": "./fonts/6a17aa3415a245dd15f4382fc0461e40.svg"
+  },
+  {
+    "url": "./fonts/69931a5f2b128d2591c643f0f19abef8.svg"
+  },
+  {
+    "url": "./fonts/b9d5ca44583e9416ee1be00c590d02f1.ttf"
+  },
+  {
+    "url": "./fonts/1db1469bf7acbd08f97df4f18046a50f.woff"
+  },
+  {
+    "url": "./fonts/2b8493494c3e0bed01e3d0db60f0c49b.woff2"
+  },
+  {
+    "url": "./fonts/3ee24eb174d3fddc8bf9e2364206b516.ttf"
+  },
+  {
+    "url": "./fonts/3ce3e2e315625e0955240b145c85c682.svg"
+  },
+  {
+    "url": "./fonts/2dbafe616960c2bbca9731b5326e788a.ttf"
+  },
+  {
+    "url": "./fonts/372d0cc3288fe8e97df49742baefce90.ttf"
+  },
+  {
+    "url": "./fonts/3426c3e0938e49036ec41bee15567cb2.woff"
+  },
+  {
+    "url": "./fonts/399a9357b6e525fb4d8e2e2786df190e.woff"
+  },
+  {
+    "url": "./fonts/36bd53ce67fc03d1ce802973ad5c1938.svg"
+  },
+  {
+    "url": "./fonts/3c3d09354e2028e0877ee77a48c0e8d7.ttf"
+  },
+  {
+    "url": "./fonts/35678ce729666262d2626facaea77e7e.svg"
+  },
+  {
+    "url": "./fonts/3c4b940f8af36c147efecdc82bbee391.svg"
+  },
+  {
+    "url": "./fonts/2d0415fa29ea596b7a02c78eddeede20.woff"
+  },
+  {
+    "url": "./fonts/01c01a5557565545312a148d5e8b0719.woff"
+  },
+  {
+    "url": "./fonts/277557a1614d9ebf11d497c62d835f88.woff"
+  },
+  {
+    "url": "./fonts/2b40783b3427f1786f1f4797ba1acbb1.svg"
+  },
+  {
+    "url": "./fonts/2b497e540c8403d6ac2811140bed841d.eot"
+  },
+  {
+    "url": "./fonts/28baf885b0b17b64b9a92bf68a0b2055.eot"
+  },
+  {
+    "url": "./fonts/29c1d31f7d9bc4f5c1841eb14fbf5cd7.eot"
+  },
+  {
+    "url": "./fonts/12832b6e9cc77a56a42b4683722062fa.eot"
+  },
+  {
+    "url": "./fonts/1e09096ff6a997de7f52c3a7eadf7413.eot"
+  },
+  {
+    "url": "./fonts/203179d16cd511feb9d8691f27926c3b.woff2"
+  },
+  {
+    "url": "./fonts/2075794c8e9e7e48e5fbf1b2313e7adf.woff2"
+  },
+  {
+    "url": "./fonts/275cdeb1f4820a4162cadddbc4b2e278.eot"
+  },
+  {
+    "url": "./fonts/198d320b73bc44e4f0dd33d6b09c9511.woff2"
+  },
+  {
+    "url": "./fonts/132e9759d93e4eefd7cdde0d7a322991.ttf"
+  },
+  {
+    "url": "./fonts/108f1f23ad45de8aeea5849592c8b685.woff2"
+  },
+  {
+    "url": "./fonts/0db749fc4928ad447dae6d6870bdcea6.svg"
+  },
+  {
+    "url": "./fonts/0bbfbc824c06da1c882639490a040ee5.woff"
+  },
+  {
+    "url": "./fonts/091e2c070a249fc3c2dd90a14ec76d38.svg"
+  },
+  {
+    "url": "./fonts/068a0d40038e7602ed364583178f3c1f.ttf"
+  }
+];
--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-importScripts("./precache-manifest.1b4fea68f984962ac35bac3eccb0be42.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("./precache-manifest.0705d5e4e671c43f9f90527bedd13623.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.skipWaiting();
 workbox.clientsClaim();
