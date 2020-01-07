de74dccb0cc4e9c9ad471aa2ee2838ef6406ab8e
salgum1114 <salgum1112@gmail.com>
2020-01-08 08:28:36 +0900

    [#145] File rename IFrame -> Iframe

--- src/components/canvas/objects/index.ts
+++ src/components/canvas/objects/index.ts
@@ -1,21 +1,11 @@
 export { default as Gif } from './Gif';
-
 export { default as Arrow } from './Arrow';
-
 export { default as Chart } from './Chart';
-
 export { default as Element } from './Element';
-
 export { default as Iframe } from './Iframe';
-
 export { default as Video } from './Video';
-
 export { default as Node } from './Node';
-
 export { default as Link } from './Link';
-
 export { default as CurvedLink } from './CurvedLink';
-
 export { default as OrthogonalLink } from './OrthogonalLink';
-
 export { default as Port } from './Port';
