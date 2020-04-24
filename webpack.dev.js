26148d0a35ad08ed69e47fb44f53601249a47b7b
Sung Gyun Oh <salgum1112@gmail.com>
2020-04-24 15:36:56 +0900

    [#164] Link bug fixes

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,49 +8,49 @@ const devPort = 8080;
 const host = 'localhost';
 
 module.exports = merge(baseConfig, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: {
-        bundle: [
-            '@babel/polyfill',
-            'react-hot-loader/patch',
-            `webpack-dev-server/client?http://${host}:${devPort}`,
-            'webpack/hot/only-dev-server',
-            path.resolve(__dirname, 'src/index.js'),
-        ],
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        publicPath: '/',
-        filename: '[name].[hash:16].js',
-        chunkFilename: '[id].[hash:16].js',
-    },
-    devServer: {
-        inline: true,
-        port: devPort,
-        contentBase: path.resolve(__dirname, 'public'),
-        hot: true,
-        publicPath: '/',
-        historyApiFallback: true,
-        host,
-        proxy: {
-            '/api': {
-                target: 'http://localhost',
-            },
-            '/api/ws': {
-                target: 'ws://localhost',
-                ws: true,
-            },
-        },
-        headers: {
-            'X-Frame-Options': 'sameorigin', // used iframe
-        },
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(), // HMR을 사용하기 위한 플러그인
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            title: 'React Design Editor',
-        }),
-    ],
+	mode: 'development',
+	devtool: 'inline-source-map',
+	entry: {
+		bundle: [
+			'@babel/polyfill',
+			'react-hot-loader/patch',
+			`webpack-dev-server/client?http://${host}:${devPort}`,
+			'webpack/hot/only-dev-server',
+			path.resolve(__dirname, 'src/index.js'),
+		],
+	},
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		publicPath: '/',
+		filename: '[name].[hash:16].js',
+		chunkFilename: '[id].[hash:16].js',
+	},
+	devServer: {
+		inline: true,
+		port: devPort,
+		contentBase: path.resolve(__dirname, 'public'),
+		hot: true,
+		publicPath: '/',
+		historyApiFallback: true,
+		host,
+		proxy: {
+			'/api': {
+				target: 'http://localhost',
+			},
+			'/api/ws': {
+				target: 'ws://localhost',
+				ws: true,
+			},
+		},
+		headers: {
+			'X-Frame-Options': 'sameorigin', // used iframe
+		},
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(), // HMR을 사용하기 위한 플러그인
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			title: 'React Design Editor',
+		}),
+	],
 });
--- webpack.prod.js
+++ webpack.prod.js
@@ -8,61 +8,56 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 const baseConfig = require('./webpack.common.js');
 
 const plugins = [
-    // 로더들에게 옵션을 넣어주는 플러그인
-    new webpack.LoaderOptionsPlugin({
-        minimize: true,
-    }),
-    // index.html 로 의존성 파일들 inject해주는 플러그인
-    new HtmlWebpackPlugin({
-        filename: 'index.html',
-        title: 'React Design Editor',
-        meta: {
-            description: `React Design Editor has started to developed direct manipulation of editable design tools like Powerpoint, We've developed it with react.js, ant.design, fabric.js`,
-        },
-    }),
-    new WorkboxPlugin.InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'sw.js',
-    }),
+	// 로더들에게 옵션을 넣어주는 플러그인
+	new webpack.LoaderOptionsPlugin({
+		minimize: true,
+	}),
+	// index.html 로 의존성 파일들 inject해주는 플러그인
+	new HtmlWebpackPlugin({
+		filename: 'index.html',
+		title: 'React Design Editor',
+		meta: {
+			description: `React Design Editor has started to developed direct manipulation of editable design tools like Powerpoint, We've developed it with react.js, ant.design, fabric.js`,
+		},
+	}),
+	new WorkboxPlugin.GenerateSW({
+		skipWaiting: true,
+		clientsClaim: true,
+		swDest: 'sw.js',
+	}),
 ];
 module.exports = merge(baseConfig, {
-    mode: 'production',
-    entry: {
-        vendor: [
-            'react',
-            'react-dom',
-            'lodash',
-            'fabric',
-            'antd',
-        ],
-        app: ['@babel/polyfill', path.resolve(__dirname, 'src/index.js')],
-    },
-    output: {
-        // entry에 존재하는 app.js, vendor.js로 뽑혀 나온다.
-        path: path.resolve(__dirname, 'docs'),
-        filename: 'js/[name].[chunkhash:16].js',
-        chunkFilename: 'js/[id].[chunkhash:16].js',
-        publicPath: './',
-    },
-    optimization: {
-        minimizer: [
-            // we specify a custom UglifyJsPlugin here to get source maps in production
-            new TerserPlugin({
-                cache: true,
-                parallel: true,
-                terserOptions: {
-                    warnings: false,
-                    compress: {
-                        warnings: false,
-                        unused: true, // tree shaking(export된 모듈 중 사용하지 않는 모듈은 포함하지않음)
-                    },
-                    ecma: 6,
-                    mangle: true,
-                    unused: true,
-                },
-                sourceMap: true,
-            }),
-        ],
-    },
-    plugins,
+	mode: 'production',
+	entry: {
+		vendor: ['react', 'react-dom', 'lodash', 'fabric', 'antd'],
+		app: ['@babel/polyfill', path.resolve(__dirname, 'src/index.js')],
+	},
+	output: {
+		// entry에 존재하는 app.js, vendor.js로 뽑혀 나온다.
+		path: path.resolve(__dirname, 'docs'),
+		filename: 'js/[name].[chunkhash:16].js',
+		chunkFilename: 'js/[id].[chunkhash:16].js',
+		publicPath: './',
+	},
+	optimization: {
+		minimizer: [
+			// we specify a custom UglifyJsPlugin here to get source maps in production
+			new TerserPlugin({
+				cache: true,
+				parallel: true,
+				terserOptions: {
+					warnings: false,
+					compress: {
+						warnings: false,
+						unused: true, // tree shaking(export된 모듈 중 사용하지 않는 모듈은 포함하지않음)
+					},
+					ecma: 6,
+					mangle: true,
+					unused: true,
+				},
+				sourceMap: true,
+			}),
+		],
+	},
+	plugins,
 });
