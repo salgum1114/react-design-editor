0e484a049f4d56a646abfa5fd1e5149012d6a8d4
Sung Gyun Oh <salgum1112@gmail.com>
2020-05-11 16:08:13 +0900

    [#164] Added 'Cube' object

--- src/components/canvas/global.d.ts
+++ src/components/canvas/global.d.ts
@@ -3,24 +3,25 @@ export {};
 type T = Window & typeof globalThis;
 
 declare module 'fabric/fabric-impl' {
-    // Common
-    class Gif {}
-    class Arrow {}
-    // Element
-    class Iframe {}
-    class Chart {}
-    class Element {}
-    class Video {}
-    // Node
-    class Node {}
-    // Link
-    class Link {}
-    class CurvedLink {}
-    class OrthogonalLink {}
+	// Common
+	class Gif {}
+	class Arrow {}
+	// Element
+	class Iframe {}
+	class Chart {}
+	class Element {}
+	class Video {}
+	// Node
+	class Node {}
+	// Link
+	class Link {}
+	class CurvedLink {}
+	class OrthogonalLink {}
+	class Cube {}
 }
 
 declare global {
-    interface Window {
-        gifler: any;
-    }
+	interface Window {
+		gifler: any;
+	}
 }
