e9da862695e80d55360e76bd2aa09d544f9601ec
Sung Gyun Oh <salgum1112@gmail.com>
2020-05-22 16:55:35 +0900

    deleted docs folder

--- scripts/ghpages.js
+++ scripts/ghpages.js
@@ -1,10 +1,20 @@
-const ghpages = required('gh-pages');
+const ghpages = require('gh-pages');
 
-ghpages.publish('docs', {
-	repo: 'https://github.com/salgum1114/react-design-editor.git',
-	message: 'auto commit',
-	user: {
-		name: 'salgum1114',
-		email: 'salgum1112@gmail.com',
+ghpages.publish(
+	'docs',
+	{
+		repo: 'https://github.com/salgum1114/react-design-editor.git',
+		message: 'published https://salgum1114.github.io/react-design-editor',
+		user: {
+			name: 'salgum1114',
+			email: 'salgum1112@gmail.com',
+		},
 	},
-});
+	function(err) {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log('published https://salgum1114.github.io/react-design-editor');
+		}
+	},
+);
